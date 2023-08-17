1e54395e9bde43de52a7f2f6dbe28601ca709d86
Amine derbal <aminedoran31@gmail.com>
2023-08-17 12:54:15 +0100

    Update component ballot to fetch and display the data

--- src/Components/Ballot/Ballot.js
+++ src/Components/Ballot/Ballot.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBallotData } from '../../redux/ballot/ballotSlice';
+import Loader from '../Loader/Loader';
+import Category from '../Category/Category';
 
 const Ballot = () => {
+  const {
+    ballotData, isFetched, isLoading, hasError,
+  } = useSelector((store) => store.ballot);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!isFetched) {
+      dispatch(fetchBallotData());
+    }
+  }, [dispatch, isFetched]);
+  if (ballotData.length === 0) {
+    return <h2 className="mt-5 text-center text-3xl font-medium">No data was fetched</h2>;
+  }
+  if (isLoading) return <Loader />;
+  if (hasError) return <h2 className="mt-5 text-center text-3xl font-medium">Error fetching data</h2>;
   return (
-    <div className='ballot'>
-      'Your Code Goes Here'
-    </div>
-  )
-}
+    <>
+      <h1 className="mt-5 text-center text-4xl font-bold">Awards 2021</h1>
+      {ballotData.map((category) => (
+        <div key={category.id}>
+          <h2 className="mt-5 text-center text-3xl font-medium">{category.id}</h2>
+          <Category category={category.items} />
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default Ballot;
